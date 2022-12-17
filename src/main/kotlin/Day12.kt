85e89f69d98a4318d58c62be8476fcd336ea15a0
Ben Papacek <benpapacek@gmail.com>
2022-12-17 18:07:33 +1100

    Day 12 part 2

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -2,13 +2,45 @@ object Day12 {
 
     private val input = FileReader().readFile("input-day12.txt")
 
-    data class Node(val row: Int, val col: Int, val c: Char, val value: Char, var neighbours: List<Node>, var dist: Int, var prev: Node?) {
+    data class Node(
+        val row: Int, val col: Int, val c: Char, val value: Char, var neighbours: List<Node>,
+        var dist: Int = Integer.MAX_VALUE, var prev: Node? = null
+    ) {
         override fun toString() = "$c($row,$col)"
         override fun equals(other: Any?) = (other as? Node)?.row == this.row && (other as? Node)?.col == this.col
         override fun hashCode() = 31 * row + col
     }
 
     fun part1() {
+        val nodes = getNodes()
+        val source = nodes.first { it.c == 'S' }
+        val target = nodes.first { it.c == 'E' }
+
+        val path = dijkstra(nodes, source, target)
+//        println(path.reversed())
+
+        // subtract one to account for the start node
+        val ans = path.size - 1
+        println("day 12 part 1: $ans")
+    }
+
+    fun part2() {
+        val originalNodes = getNodes()
+
+        // add a dummy node for the source
+        val source = Node(-1, -1, 'S', 'A', originalNodes.filter { it.value == 'a' })
+        val target = originalNodes.first { it.c == 'E' }
+        val nodes = originalNodes + listOf(source)
+
+        val path = dijkstra(nodes, source, target)
+//        println(path.reversed())
+
+        // now subtract two to account for the dummy 'start' node and the start node itself
+        val ans = path.size - 2
+        println("day 12 part 2: $ans")
+    }
+
+    private fun getNodes(): List<Node> {
         val nodeGrid = input.split("\n").filterNot { it.isBlank() }.mapIndexed { i, line ->
             line.toCharArray().mapIndexed { j, c ->
                 val elevation = when (c) {
@@ -16,10 +48,9 @@ object Day12 {
                     'E' -> 'z'
                     else -> c
                 }
-                Node(i, j, c, elevation, listOf(), Integer.MAX_VALUE, null)
+                Node(i, j, c, elevation, listOf())
             }
         }
-
         (0 .. nodeGrid.indices.last).forEach { i ->
             (0 .. nodeGrid[i].indices.last).forEach { j ->
                 val node = nodeGrid[i][j]
@@ -31,13 +62,7 @@ object Day12 {
                 node.neighbours = listOfNotNull(north, south, east, west)
             }
         }
-        val nodes = nodeGrid.flatten().toMutableList()
-        val source = nodes.first { it.c == 'S' }
-        val target = nodes.first { it.c == 'E' }
-
-        val shortestPath = dijkstra(nodes, source, target)
-        val ans = shortestPath.size - 1
-        println("day 12 part 1: $ans")
+        return nodeGrid.flatten()
     }
 
     // Dijkstra's algorithm https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
